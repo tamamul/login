14380ff1b0ff0e85ac77f33568af86d1a3220917
tamamul <36046825+tamamul@users.noreply.github.com>
2026-06-06 17:25:00 +0700

    Add app constants

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,13 @@
+class AppConstants {
+  static const String appName       = 'Marsa CBT';
+  static const String packageName   = 'com.marsa9.cbt';
+
+  // Heartbeat interval
+  static const int heartbeatSeconds = 10;
+
+  // Violation threshold
+  static const int violationLimit   = 3;
+
+  // Autosave delay
+  static const int autosaveMs       = 500;
+}
